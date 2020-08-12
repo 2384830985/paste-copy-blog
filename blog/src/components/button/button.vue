c78344146c29db20205b650bda0bf5d4ce904b43
jinxintang <jinxintang@iyuedian.com>
2020-08-12 17:35:51 +0800

    项目创建

---
+++ blog/src/components/button/button.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <pc-button type="success" :disabled="disabled" :loading="disabled" @click="disabled = true">
+            <span v-if="!disabled">Click me!</span>
+            <span v-else>Loading...</span>
+        </pc-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "buttons",
+        data(){
+            return {
+                disabled: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- blog/src/main.js
+++ blog/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Pc from 'paste-copys'
+import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Pc)
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router,
 }).$mount('#app')
