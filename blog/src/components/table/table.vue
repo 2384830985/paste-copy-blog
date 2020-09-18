7dc44f89fda17957a284aa2e64b2713f38bd2bdb
jinxintang <jinxintang@iyuedian.com>
2020-09-18 15:11:47 +0800

    添加table

--- blog/src/components/table/table.vue
+++ blog/src/components/table/table.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <component-title title="table"
-                         small="普通table" />
+                         small="功能展示" />
         <code-block :code="tableCode">
             <template slot="demo">
                 <pc-table :columns="columns"
                           :data="tableData"
+                          :spanMethod="spanMethod"
+                          border
                           stripe
                           height="400"
+                          :rowClassName="rowClassName"
                 ></pc-table>
             </template>
         </code-block>
@@ -16,20 +19,54 @@
             <template slot="demo">
                 <pc-table :columns="columns2"
                           :data="tableData"
-                          stripe
                           border
                 ></pc-table>
             </template>
         </code-block>
-        <component-title small="多级表头table" />
+        <component-title small="loading" />
+        <pc-table :columns="columns2"
+                  :data="tableData"
+                  loading
+                  stripe
+                  border
+        ></pc-table>
+        <file-text :ApiEventData="ApiEventData" :ApiData="ApiData"/>
+        <file-text :ApiData="columnsData" ApiEventDataTitle="columns"/>
     </div>
 </template>
 
 <script>
+    import FileText from "../../componentsUilst/fileText";
     export default {
         name: "tables",
+        components: {FileText},
         data(){
             return {
+                ApiEventData: [
+                    this.$utils.setApiEventData('on-sort','上下排序进行触发', ''),
+                    this.$utils.setApiEventData('checkboxChange','单个checkbox触发', 'tableData,checkbox 所有数据和是否选中'),
+                    this.$utils.setApiEventData('checkboxChangeHead','header上选中所有checkbox触发', 'tableData,item,index 选中数据和当前行和当前行下标'),
+                ],
+                columnsData: [
+                    this.$utils.setApiData('title', '当前名称', 'String', ''),
+                    this.$utils.setApiData('type', '类型：expand|index|selection', 'String', ''),
+                    this.$utils.setApiData('width', '宽度', 'Number', ''),
+                    this.$utils.setApiData('key', '当前的data的key', 'Number', ''),
+                    this.$utils.setApiData('align', '位置：center|left|right', 'Number', ''),
+                    this.$utils.setApiData('children', '可以进行一个嵌套', 'Object', ''),
+                    this.$utils.setApiData('sort', '是否需要排序', 'Boolean', ''),
+                    this.$utils.setApiData('fixed', '是否要靠左或者靠右left|right', 'Boolean', ''),
+                ],
+                ApiData: [
+                    this.$utils.setApiData('stripe', '添加 # 后面会自动排序', 'Boolean', 'false'),
+                    this.$utils.setApiData('border', '是否添加边框', 'Boolean', 'false'),
+                    this.$utils.setApiData('loading', '是否添加loading', 'Boolean', 'false'),
+                    this.$utils.setApiData('height', '高度', 'Number', ''),
+                    this.$utils.setApiData('rowClassName', '单行Class', 'Function', ''),
+                    this.$utils.setApiData('spanMethod', '处理合并单元行', 'Function', '{row,column,rowIndex,columnIndex} row 当前行数据 column 当前行的  column rowIndex 当前行的 index columnIndex 当前列的 index'),
+                    this.$utils.setApiData('columns', '当前table的内容', 'Object', ''),
+                    this.$utils.setApiData('data', '当前table的数据', 'Object', ''),
+                ],
                 tableCode: `
                 <pc-table :columns="columns"
                           :data="tableData"
@@ -51,11 +88,30 @@
                         align: 'center'
                     },
                     {
-                        title: '789',
+                        type: 'expand',
+                        width: 45,
+                        align: 'center',
+                        render: (h, { row, index }) => {
+                            return h('div','支持render 注入写法')
+                        }
+                    },
+                    {
+                        title: '支持render 注入写法，支持多层级操作',
                         key: 'name2',
                     },
                     {
-                        title: '1111',
+                        title: '支持上下筛选（只抛出事件）',
+                        key: 'name2',
+                        sort: true,
+                        order: 'top',
+                        slotSort: 'name'
+                    },
+                    {
+                        title: '支持横坐标合并',
+                        key: 'name3',
+                    },
+                    {
+                        title: '支持纵坐标合并',
                         key: 'name3',
                     },
                     {
@@ -63,11 +119,42 @@
                         key: 'name3',
                     },
                     {
-                        title: '1111',
-                        key: 'name3',
+                        title: '支持多层级嵌套table',
+                        key: 'name',
+                        align: 'center',
+                        children: [
+                            {
+                                title: '多级表头可以加滚动',
+                                children: [
+                                    {
+                                        title: '可以取消边框',
+                                        key: 'Address1',
+                                        children: [
+                                            {
+                                                title: 'Address1',
+                                                key: 'Address1',
+                                            },
+                                        ]
+                                    },
+                                    {
+                                        title: 'Address2',
+                                        key: 'Address2',
+                                    },
+                                ]
+                            },
+                            {
+                                title: '可以对单个框进行class 的赋值',
+                                key: 'Address123456789',
+                            },
+                        ]
                     },
                     {
-                        title: '1111',
+                        type: 'selection',
+                        width: 48,
+                        align: 'center',
+                    },
+                    {
+                        title: '添加选中',
                         key: 'name3',
                     },
                 ],
@@ -142,10 +229,63 @@
                     },
                 ]
             }
+        },
+        methods: {
+            rowClassName({row,index,data}){
+                // console.log(row)
+                // console.log(index)
+                // console.log(data)
+                if (index===2) {
+                    return 'red'
+                }
+            },
+            spanMethod({row,column,rowIndex,columnIndex}){
+                // console.log(row)
+                // console.log(rowIndex)
+                // console.log(columnIndex)
+                if (columnIndex === 4) {
+                    if (rowIndex % 3 === 0) {
+                        return {
+                            rowspan: 3,
+                            colspan: 1
+                        };
+                    } else {
+                        return {
+                            rowspan: 0,
+                            colspan: 0
+                        };
+                    }
+                }
+                if (rowIndex % 2 === 0) {
+                    if (columnIndex === 0) {
+                        return {
+                            rowspan: 1,
+                            colspan: 3
+                        };
+                    } else if (columnIndex === 1) {
+                        return {
+                            rowspan: 0,
+                            colspan: 0
+                        };
+                    }else if (columnIndex === 2) {
+                        return {
+                            rowspan: 0,
+                            colspan: 0
+                        };
+                    }else {
+                        return {
+                            rowspan: 1,
+                            colspan: 1
+                        };
+                    }
+                }
+            }
         }
     }
 </script>
 
-<style scoped>
-
+<style>
+    .red{
+        background: red;
+    }
 </style>
--- blog/src/componentsUilst/fileText.vue
+++ blog/src/componentsUilst/fileText.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
-        <component-title title="API"
+        <component-title :title="ApiEventDataTitle"
+                         v-if="ApiData.length"
                          small="具体属性" />
         <pc-table :columns="ApiColumns"
                   :data="ApiData"
+                  v-if="ApiData.length"
         >
         </pc-table>
-        <component-title title="事件" />
+        <component-title :title="ApiDataTitle"  v-if="ApiEventData.length" />
         <pc-table :columns="ApiEvent"
                   :data="ApiEventData"
+                  v-if="ApiEventData.length"
         >
-            <span slot="name">123</span>
+            <span slot="name"></span>
             <div slot="111"></div>
         </pc-table>
     </div>
@@ -33,6 +36,14 @@
             ApiEventData: {
                 type: Array,
                 default: []
+            },
+            ApiDataTitle: {
+                type: String,
+                default: "事件"
+            },
+            ApiEventDataTitle: {
+                type: String,
+                default: "API"
             },
             ApiData: {
                 type: Array,
